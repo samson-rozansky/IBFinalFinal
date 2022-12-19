d05768223fc84c7eee85fe03e61c407693278dd6
samson-rozansky <66850179+samson-rozansky@users.noreply.github.com>
2022-12-18 20:13:17 -0500

    Create SuperMain.java

---
+++ SuperMain.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.time.*;
+import java.io.*;
+
+public class SuperMain{
+    public static ArrayList<Notes> NoteList;
+    public static HashMap<Character, Integer> CharacterFrequency;
+
+    public static void main(String[] args) throws IOException {
+        File TheNotes = new File("notes.txt");
+        TheNotes.delete();
+        NoteList = new ArrayList<Notes>();
+        CharacterFrequency = new HashMap<Character, Integer>();
+        new Main();
+    }
+}
